f4c7be30d1b3ba2fdd86b89d9f90e752580670ed
s4shresthasujan <s4shresthasujan@gmail.com>
2026-08-07 15:00:34 +0545

    Fix webpack publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ export default {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
+    publicPath: "/restaurantSite/",
   },
   devtool: "eval-source-map",
   devServer: {
